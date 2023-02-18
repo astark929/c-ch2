5f695d89eb1a125fda70a8765cb3aed7b2cf1f34
Aaron <33184112+astark929@users.noreply.github.com>
2023-02-17 21:26:52 -0800

    Update data.h

--- data.h
+++ data.h
@@ -1,4 +1,4 @@
-const char* title = "\n\t\t\t ***** < Miramar College Book Ordering System > *****   ";
+const char* title = "               ***** < Miramar College Book Ordering System > *****   ";
 
 const char* instructions = "please put in a number for the calculations the press enter";
 
@@ -6,16 +6,5 @@ const char* request = "please enter your number ";
 
 const char* result = "your result is ";
 
-const char* output_screen = "\t\t\t\t output screen";
-
-
-
-const char* quantity = "100";
-
-const char* taxable = "y";
-
-const char* handling = "y";
-
-const char* price = "$10.0";
-
+const char* output_screen = "\t\t\t\t\t output screen";
 
