4924c02b42e8f4aa0e862682957c2e2b0e211a5f
Aaron <33184112+astark929@users.noreply.github.com>
2023-02-17 21:27:33 -0800

    Update title.h

--- title.h
+++ title.h
@@ -1,4 +1,5 @@
-const char* stuff = "\t\t\t                     TITLE SCREEN                    "
-            "\n\t\t\t ***** < Miramar College Book Ordering System > *****   "
-            "\n\t\t\t                 2/17/2023 Aaron Goff             "
-            "\n\n\t\t\t            please press any key to continue";    
+const char* stuff = 
+"                                            TITLE SCREEN                    \n"
+"                        ***** < Miramar College Book Ordering System > *****   \n"
+"                                       2/17/2023 Aaron Goff             \n"
+"                                please press 'ENTER' to continue";    
