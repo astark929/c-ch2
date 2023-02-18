402ecdee4b6d9653108667d56b7a4ef782969bad
Aaron <33184112+astark929@users.noreply.github.com>
2023-02-17 21:26:23 -0800

    Update calc.h

--- calc.h
+++ calc.h
@@ -1,16 +1,63 @@
 #ifndef values
 #define values
+#include <string>
+using namespace std;
 
-double calculation(double price, char tax, char handle) {
-    double final_price = price;
-    if (tax == 'y'){
-        final_price = final_price * 1.0775;
+double calculation(double price, int enrollment, string required, string book){
+    double numBooks;
+    double factored = 1;
+    if(book == "new"){
+        if(required == "required"){
+         factored = 1.0;   // Book is new and required
+        }
+        else{
+         factored = 0.35;  // Book is new but not required
+        }
     }
-    if (handle == 'y')
-    {
-        final_price = final_price * 1.10;
+    else {
+        if(required == "required"){
+            factored = 0.6;  // Book is not new but is required
+        }
+        else  {
+            factored = 0.1;  // Book is not new and is not required
+        }
     }
-    return final_price;
+    numBooks= enrollment * factored;
+    double cost = numBooks * price;
+    return cost;
+}
+
+int amountofbooks (int enrolled, string required, string book){
+    double numBooks = enrolled;
+    double factored = 1;
+    if(book == "new"){
+        if(required == "required"){
+         factored = 1.0;   // Book is new and required
+        }
+        else{
+         factored = 0.35;  // Book is new but not required
+        }
+    }
+    else {
+        if(required == "required"){
+            factored = 0.6;  // Book is not new but is required
+        }
+        else  {
+            factored = 0.1;  // Book is not new and is not required
+        }
+    }
+    numBooks= enrolled * factored;
+    return numBooks;
+}
+
+double check(string input){
+    double price = atof(input.c_str());
+    return price;
+}
+
+int stringtoint(string amount){
+    int newamount = std::atoi(amount.c_str());
+    return newamount;
 }
 
 
